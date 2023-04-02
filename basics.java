9753b08ed33dd702590bc6dead69b637642cfed4
Tyrah <tyrah96@gmail.com>
2023-04-02 15:14:56 -0700

    arithmetic operators

--- basics.java
+++ basics.java
@@ -20,6 +20,26 @@ class Main {
         char copyrightSymbol = '\u00A9';
 
         System.out.println("This is a copyright symbol: " + copyrightSymbol);
+
+
+        // arithmetic operators
+        int number1 = 12;
+        int number2 = 6;
+
+        // addition
+        System.out.println(number1 + number2); // 18
+
+        // subtraction 
+        System.out.println(number1 - number2); // 6
+
+        // multiplication
+        System.out.println(number1 * number2); // 72
+
+        // division
+        System.out.println(number1 / number2); // 2
+
+        // remainder (modulo/modulus)
+        System.out.println(number1 % number2); // 0
     }
 
 }
